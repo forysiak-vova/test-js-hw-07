6085e855519500a6f3d7b3bb0037d9cbb82b3e5e
forysiak-vova <volodja.forus@gmail.com>
2021-11-06 09:15:33 +0100

    s

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// import validePassword from './js/valide';
+// import {fetchAllUsers,  fetchUsersById, uppdateUsersById, x as value,y as name} from './js/api.servises.js'
+import * as qwe from './js/api.servises.js'
+// console.log(validePassword('qweqweqwe'));
+// console.log(fetchAllUsers);
+// console.log(fetchUsersById);
+// console.log(uppdateUsersById);
+// console.log(value);
+// console.log(name);
+console.log(qwe);
---
+++ js/api.servises.js
@@ -0,0 +1,11 @@
+export const fetchAllUsers = () => {
+   console.log('fetchAllUsers');
+};
+export const fetchUsersById = (id) => {
+   console.log('fetchUsersById');
+};
+export const uppdateUsersById = (id) => {
+   console.log('uppdateUsersById');
+};
+export const x = 5;
+export const y = 'manga';
